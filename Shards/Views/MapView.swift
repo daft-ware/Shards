9a2a57d9d0b6f918b6c3a4a267b800f1441d5753
scraptechguy <rostislav.bro@gmail.com>
2021-11-08 12:21:00 +0100

    Create MapView.swift

---
+++ Shards/Views/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Shards
+//
+//  Created by Rostislav Brož on 11/8/21.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
