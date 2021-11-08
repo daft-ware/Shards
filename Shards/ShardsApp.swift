2874b4d95ceb716ef4a26af8910bfecc1734f248
scraptechguy <rostislav.bro@gmail.com>
2021-11-08 11:53:14 +0100

    Initial Commit

---
+++ Shards/ShardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShardsApp.swift
+//  Shards
+//
+//  Created by Rostislav Brož on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ShardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
