20c62ede31b8112d5a42e415316de590afd7ba7b
scraptechguy <rostislav.bro@gmail.com>
2021-11-08 12:41:30 +0100

    Update LaunchView.swift

--- Shards/Views/LaunchView.swift
+++ Shards/Views/LaunchView.swift
@@ -24,7 +24,7 @@ struct LaunchView: View {
         ZStack {
             white.ignoresSafeArea()
                     
-            Text("Done")
+            Text("Done boiii")
         }
     }
 }
