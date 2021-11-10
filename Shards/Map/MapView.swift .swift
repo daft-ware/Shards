0182d5efb36717e10d6c802f0fdd91650e70b26c
scraptechguy <rostislav.bro@gmail.com>
2021-11-10 17:15:32 +0100

    Create Map group with MapView.swift in it

---
+++ Shards/Map/MapView.swift .swift
@@ -0,0 +1,21 @@
+//
+//  MapView.swift .swift
+//  Shards
+//
+//  Created by Rostislav Brož on 11/10/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView_swift_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_swift__Previews: PreviewProvider {
+    static var previews: some View {
+        MapView_swift_()
+    }
+}
